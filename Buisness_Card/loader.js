3890a2583da3a48bd7e312b88b35a1aa44d4b588
rudrakashchoubey <rudrachoubey70@gmail.com>
2026-02-24 14:50:36 +0530

    Mini Projects

---
+++ Buisness_Card/loader.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("main-content").style.display = "block";
+    }, 2000);
+});
